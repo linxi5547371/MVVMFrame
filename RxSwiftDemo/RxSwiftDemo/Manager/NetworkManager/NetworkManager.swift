6be9aeb19eb2be1f2ab35ac5aec6e7bd5faae434
方梦凯 <mengkai.fang@qingclass.com>
2019-11-25 18:05:03 +0800

    网络请求发送设置更新

--- RxSwiftDemo/RxSwiftDemo/Manager/NetworkManager/NetworkManager.swift
+++ RxSwiftDemo/RxSwiftDemo/Manager/NetworkManager/NetworkManager.swift
@@ -9,10 +9,23 @@
 import Foundation
 import Alamofire
 
+extension SessionManager {
+    public static let `customConfig` : SessionManager = {
+        let configuration = URLSessionConfiguration.default
+        configuration.httpAdditionalHeaders = SessionManager.defaultHTTPHeaders
+        
+        // 30s超时
+        configuration.timeoutIntervalForRequest = 30
+        
+        return SessionManager(configuration: configuration)
+    }()
+}
+
 struct NetworkManager<request: BaseRequet> {
     
     static func sendRequest(request: request, compeletionHandler: @escaping ((request.response?) -> Void)) {
-        Alamofire.request(request.requestURL, method: request.method, parameters: request.parame, encoding: request.encoding, headers: request.headerFields).response { response in
+        let sessionManager = SessionManager.customConfig
+        sessionManager.request(request.requestURL, method: request.method, parameters: request.parame, encoding: request.encoding, headers: request.headerFields).response { response in
             guard let data = response.data else { return }
             compeletionHandler(request.decodeResponse(data: data))
         }
